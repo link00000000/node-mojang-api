fc1d96f8554efe5fda8b5d19c9bcc28dce23ae6e
link00000000 <crandall.logan@gmail.com>
2019-05-26 18:47:14 -0400

    authentication constructor

---
+++ index.js
@@ -0,0 +1,9 @@
+/**
+ * Says hello
+ * @param {string} person - The person to say hello to
+ */
+ function sayHello(person) {
+     console.log(`Hello ${person}!`);
+ }
+
+ sayHello("World");
--- test/test.js
+++ test/test.js
@@ -1,5 +1,8 @@
 import test from "ava";
 
-test("foo", t => {
+test("authentication", t => {
+    const Authentication = require('../lib/Authentication');
+    let client = new Authentication("email", "password");
+    console.log(client);
     t.pass();
 });
