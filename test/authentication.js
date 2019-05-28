f585b19561d4800d7438f89ac50bdc26d8f7e54a
link00000000 <crandall.logan@gmail.com>
2019-05-28 18:34:00 -0400

    Authentication/authenticate

---
+++ test/authentication.js
@@ -0,0 +1,15 @@
+const test = require("ava");
+require('dotenv').config();
+
+const Authentication = require('../lib/Authentication');
+
+test("Authentication/constructor", t => {
+    let client = new Authentication(process.env.EMAIL, process.env.PASSWORD);
+    t.pass();
+});
+
+test("Authentication/authenticate", async t => {
+    let client = new Authentication(process.env.EMAIL, process.env.PASSWORD);
+    let res = await client.authenticate();
+    t.pass();
+});
