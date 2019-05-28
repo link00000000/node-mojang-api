c0045a8375b64a96d5b772355983c04fa4a982eb
link00000000 <crandall.logan@gmail.com>
2019-05-28 13:32:52 -0400

    removed test/test.js

--- test/test.js
+++
@@ -1,8 +0,0 @@
-import test from "ava";
-
-test("authentication", t => {
-    const Authentication = require('../lib/Authentication');
-    let client = new Authentication("email", "password");
-    console.log(client);
-    t.pass();
-});
