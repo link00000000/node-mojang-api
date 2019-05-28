b59dc2e3c59523f4f54d4b360cefed1825af8ea6
link00000000 <crandall.logan@gmail.com>
2019-05-28 19:16:57 -0400

    added more tests for client

--- test/client.js
+++ test/client.js
@@ -7,3 +7,32 @@ test("Client/constructor", t => {
     let client = new Client(path.join(__dirname, "client.json"));
     t.pass();
 });
+
+test("Client/fileExists", t => {
+    let clientExists = new Client(path.join(__dirname, "client.json"));
+    let clientNotExists = new Client(path.join(__dirname, "client_not_exist.json"));
+    if(clientExists.fileExists() == false)
+    {
+        t.fail();
+    }
+    if(clientNotExists.fileExists() == true)
+    {
+        t.fail();
+    }
+    t.pass();
+});
+
+test("Client/readFile", t => {
+    let client = new Client(path.join(__dirname, "client.json"));
+    client.readFile();
+    t.pass();
+});
+
+test("Client/writeFile", t => {
+    let client = new Client(path.join(__dirname, "client.json"));
+    client.setAccessToken = "ACCESS_TOKEN";
+    client.setName = "NAME";
+    client.setId = "ID";
+    client.writeFile();
+    t.pass();
+});
